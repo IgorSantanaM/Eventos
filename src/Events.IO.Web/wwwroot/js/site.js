393435f68245ebb9a90bd06703bea04d13e16df9
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-06-02 13:47:36 -0300

    (feat): Added Address and their validations

--- src/Events.IO.Web/wwwroot/js/site.js
+++ src/Events.IO.Web/wwwroot/js/site.js
@@ -1,9 +1,77 @@
-﻿function EventVlidations(){
+﻿function EventValidations() {
     $.validator.methods.range = function (price, element, param) {
         var globalizedPrice = price.replace(",", ".");
         return this.optional(element) || (globalizedPrice >= param[0] && globalizedPrice <= param[1]);
+    };
+
+    $.validator.methods.number = function (value, element) {
+        return this.optional(element) || /-?(?:\d+|\d{1,3}(?:[\s\.,]\d{3})+)(?:[\.,]\d+)?$/.test(value);
+    };
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
     }
-    $.validator.methods.number = function (price, element) {
-        return.this.optional(element) || /-?(?:\d+|\d{1,3}(?:[\s\.,]\d{3})+)(?:[\.,]\d+)?$/.test(price);
-    }
+
+    $('#BeginDate').datepicker({
+        format: "mm/dd/yyyy",
+        startDate: "tomorrow",
+        language: "en-US",
+        orientation: "bottom right",
+        autoclose: true
+    });
+
+    $('#EndDate').datepicker({
+        format: "mm/dd/yyyy",
+        startDate: "tomorrow",
+        language: "en-US",
+        orientation: "bottom right",
+        autoclose: true
+    });
+
+    $(document).ready(function () {
+        var $inputOnline = $("#Online");
+        var $inputFree = $("#Free");
+
+        ShowAddress();
+        ShowPrice();
+
+        $inputOnline.click(function () {
+            ShowAddress();
+        });
+
+        $inputFree.click(function () {
+            ShowPrice();
+        });
+
+        function ShowAddress() {
+            if ($inputOnline.is(":checked")) {
+                $("#AddressForm").hide();
+            } else {
+                $("#AddressForm").show();
+            }
+        }
+
+        function ShowPrice() {
+            if ($inputFree.is(":checked")) {
+                $("#Price").val("0");
+                $("#Price").prop("disabled", true);
+            } else {
+                $("#Price").val("");
+                $("#Price").prop("disabled", false);
+            }
+        }
+    });
 }
